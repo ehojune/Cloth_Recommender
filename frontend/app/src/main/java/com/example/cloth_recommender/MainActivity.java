fc126b05f46bd891fa7f9091d903345ea40d81f4
ehojune <ehojune@gmail.com>
2021-07-09 19:50:41 +0900

    tab icon implemented

--- frontend/app/src/main/java/com/example/cloth_recommender/MainActivity.java
+++ frontend/app/src/main/java/com/example/cloth_recommender/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.cloth_recommender.Frag1.Frag1;
 import com.example.cloth_recommender.Frag2.Frag2;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -37,8 +39,26 @@ public class MainActivity extends AppCompatActivity {
         //ViewPager과 TabLayout 연결
         tabLayout.setupWithViewPager(viewPager);
 
-        tabLayout.getTabAt(0).setText("첫 번째");
-        tabLayout.getTabAt(1).setText("두 번째");
-        tabLayout.getTabAt(2).setText("세 번째");
+        //tabLayout.getTabAt(0).setText("첫 번째");
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_baseline_home_24);
+        //tabLayout.getTabAt(1).setText("두 번째");
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_baseline_bookmark_24);
+        //tabLayout.getTabAt(2).setText("세 번째");
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_baseline_person_24);
+        //View view1 = getLayoutInflater().inflate(R.layout.custom_tabicon, null);
+        //view1.findViewById(R.id.icon).setBackgroundResource(R.drawable.tabicon_home);
+        //tabLayout.addTab(tabLayout.newTab().setCustomView(view1));
+
+
+        //View view2 = getLayoutInflater().inflate(R.layout.custom_tabicon, null);
+        //view2.findViewById(R.id.icon).setBackgroundResource(R.drawable.tabicon_saved);
+        //tabLayout.addTab(tabLayout.newTab().setCustomView(view2));
+
+
+        //View view3 = getLayoutInflater().inflate(R.layout.custom_tabicon, null);
+        //view3.findViewById(R.id.icon).setBackgroundResource(R.drawable.tabicon_mypage);
+        //tabLayout.addTab(tabLayout.newTab().setCustomView(view3));
+
+
     }
 }
